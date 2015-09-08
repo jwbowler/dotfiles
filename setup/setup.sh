f889359abb4551ec88d6a1dd9ed60607bbb1b0b4
John Bowler <jwbowler@gmail.com>
2015-09-07 21:02:14 -0700

    fixed chsh command in ohmyz.sh; added master setup.sh

---
+++ setup/setup.sh
@@ -0,0 +1,17 @@
+# Get zsh
+sudo apt-get install zsh
+
+# Setup ohmyzsh
+sh ~/.dotfiles/setup/ohmyz.sh
+
+# Add .*rc symlinks to home directory
+rm -rf ~/.zshrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+
+# Get Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Vundle packages in vimrc
+ssh -c "VundleInstall" .zshrc
+
